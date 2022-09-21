47c76848334000571785c1ce7d01717b0886484f
detb <57094519+detb@users.noreply.github.com>
2022-09-21 15:06:49 +0200

    endpoint change.

--- src/app/services/lp-rest.service.ts
+++ src/app/services/lp-rest.service.ts
@@ -14,7 +14,7 @@ export class LpRestService {
 
   getTasks(userId: string): Observable<LPTask[]> {
     if (userId != null) {
-      let apiUrl = `${baseUrl}tasks/${userId}`;
+      let apiUrl = `${baseUrl}tasks?user=${userId}`;
       return this.http.get<any>(apiUrl);
     }
     return NEVER;
