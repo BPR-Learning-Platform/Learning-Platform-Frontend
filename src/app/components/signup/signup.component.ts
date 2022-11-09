0bb2a0578c4fa7030b5b8ce04e962b03d57f889f
detb <57094519+detb@users.noreply.github.com>
2022-11-09 12:51:47 +0100

    Finalized possibilty to create new user :)

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthenticationService} from "../../services/authentication.service";
 import {Router} from "@angular/router";
 import {AllGrades} from "../../models/user.model";
+import {LpRestService} from "../../services/lp-rest.service";
 
 @Component({
   selector: 'app-signup',
@@ -13,24 +14,31 @@ import {AllGrades} from "../../models/user.model";
 export class SignupComponent implements OnInit {
 
   private emailPattern = "[a-z_A-Z0-9._%+-]+@[a-z_A-Z0-9.-]+\.[a-z_A-Z]{2,4}$";
-  grades: AllGrades[] = [{name: "test", id: 2}, {name: "test2", id: 23}]
+  grades: AllGrades[] = [];
   selection: any;
   status: any | null = null;
+  assignedGrades = new FormControl('', [Validators.required]);
+
 
   signupForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(4)]),
     email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    type: new FormControl('', [Validators.required]),
-    assignedGrades: new FormControl('', [Validators.required])
+    type: new FormControl('', [Validators.required])
   });
 
   constructor(
     private authenticationService: AuthenticationService,
+    private lpRestService: LpRestService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.getGrades();
+  }
+
+  typeSelection() {
+    this.assignedGrades = new FormControl('', [Validators.required]);
   }
 
   get nameControl(): FormControl {
@@ -49,27 +57,26 @@ export class SignupComponent implements OnInit {
     return this.signupForm.get('type') as FormControl;
   }
 
-  get assignedGradesControl(): FormControl {
-    return this.signupForm.get('assignedGrades') as FormControl;
-  }
-
   get f() { return this.signupForm.controls; }
+
+  getGrades(): void {
+    this.lpRestService.getAllGrades().subscribe(res => {
+      this.grades = res;
+    });
+  }
 
   signup(): void {
     let name = this.f['name'].value
     let email = this.f['email'].value
     let password = this.f['password'].value
     let type = this.f['type'].value
-    let assignedGrades = this.f['assignedGrades'].value
-    let grades = [Number(assignedGrades)];
-
     if (name != null && email != null && password != null && type != null) {
       let user = {
         name: name,
         email: email,
         password: password,
         type: type,
-        assignedGradeIds: grades,
+        assignedGradeIds: this.assignedGrades.value,
       }
 
       this.authenticationService.signup(user).subscribe(r => {
@@ -80,4 +87,5 @@ export class SignupComponent implements OnInit {
         });
     }
   }
+
 }
