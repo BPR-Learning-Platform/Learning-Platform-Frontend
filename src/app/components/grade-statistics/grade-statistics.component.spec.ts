b57579e46d2c2b4019afc036ffe8a9db6dcdddbe
detb <57094519+detb@users.noreply.github.com>
2022-11-16 13:35:47 +0100

    test fix

--- src/app/components/grade-statistics/grade-statistics.component.spec.ts
+++ src/app/components/grade-statistics/grade-statistics.component.spec.ts
@@ -1,13 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GradeStatisticsComponent } from './grade-statistics.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "../../app-routing.module";
+import {LpRestService} from "../../services/lp-rest.service";
+import {AuthenticationService} from "../../services/authentication.service";
+import {BaseChartDirective} from "ng2-charts";
 
 describe('GradeStatisticsComponent', () => {
   let component: GradeStatisticsComponent;
   let fixture: ComponentFixture<GradeStatisticsComponent>;
 
   beforeEach(async () => {
+    localStorage.setItem("user",
+      JSON.stringify({
+        name: "batman is my name",
+        email: "em",
+        type: "T",
+        assignedGradesIds: [2, 3],
+        score: 0,
+        userId: "221"
+      }));
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, AppRoutingModule ],
+      providers: [LpRestService, AuthenticationService, BaseChartDirective],
       declarations: [ GradeStatisticsComponent ]
     })
     .compileComponents();
@@ -19,5 +35,6 @@ describe('GradeStatisticsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    localStorage.removeItem("user");
   });
 });
